88bdbd3f7f1531b3777a39d1b176cdecc89e7de6
LarenuFrehtonA <emre.erdem034@hotmail.com>
2022-06-14 19:21:15 +0300

    Create sp4m.py

---
+++ sp4m.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+
+message = input("message? : ")
+str = input("How many times ?: ")
+
+print("sp4m")
+
+count = 5
+while(count != 0):
+	print(count)
+	time.sleep(1)
+	count -= 1
+
+print("sending..")
+
+for i in range(0,int(str)):
+	pyautogui.typewrite(message + '\n')
